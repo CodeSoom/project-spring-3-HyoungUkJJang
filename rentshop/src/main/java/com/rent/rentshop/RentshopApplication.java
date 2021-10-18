dab37d83dc412ec96ff278568b06c376aed6cbc2
HyoungUkJJang <guddnr1451@gmail.com>
2021-10-18 15:49:26 +0900

    ADD (10/18) 개발환경 세팅

---
+++ rentshop/src/main/java/com/rent/rentshop/RentshopApplication.java
@@ -0,0 +1,13 @@
+package com.rent.rentshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentshopApplication.class, args);
+	}
+
+}
